334605fc7f9795fb3ba03de6dc74f9ba8ca55a31
DmitryMiroshnichenko <dmir1988@gmail.com>
2015-01-24 17:53:43 +0300

    jira 6 support

--- src/main/java/ru/mail/plugins/clipcopier/CopyToClipboardCFType.java
+++ src/main/java/ru/mail/plugins/clipcopier/CopyToClipboardCFType.java
@@ -1,70 +1,41 @@
 package ru.mail.plugins.clipcopier;
 
+import java.util.Map;
+
 import com.atlassian.jira.issue.Issue;
 import com.atlassian.jira.issue.customfields.impl.CalculatedCFType;
 import com.atlassian.jira.issue.customfields.impl.FieldValidationException;
 import com.atlassian.jira.issue.fields.CustomField;
 import com.atlassian.jira.issue.fields.layout.field.FieldLayoutItem;
-import com.atlassian.sal.api.ApplicationProperties;
 
-import org.apache.commons.lang.StringEscapeUtils;
-import java.util.Map;
-
-/**
- * @author grundic
- */
-public class CopyToClipboardCFType
-    extends CalculatedCFType
-{
-    private final ApplicationProperties applicationProperties;
-    
-    public CopyToClipboardCFType(ApplicationProperties applicationProperties)
-    {
-        this.applicationProperties = applicationProperties;
+@SuppressWarnings("rawtypes")
+public class CopyToClipboardCFType extends CalculatedCFType {
+    public CopyToClipboardCFType() {
     }
-    
-    public Object getSingularObjectFromString(String str)
-    throws FieldValidationException
-    {
+
+    public Object getSingularObjectFromString(String str) throws FieldValidationException {
         return str;
     }
 
-    public String getStringFromSingularObject(Object o)
-    {
-        if (o != null)
-        {
+    public String getStringFromSingularObject(Object o) {
+        if (o != null) {
             return o.toString();
         }
         return null;
     }
 
-    public Object getValueFromIssue(
-        CustomField customField,
-        Issue issue)
-    {
-        if (issue != null && issue.getKey() != null)
-        {
-            StringBuilder sb = new StringBuilder();
-            sb.append(applicationProperties.getBaseUrl());
-            sb.append("/browse/");
-            sb.append(issue.getKey());
-            
-            return sb.toString();
-        }
-        else
-        {
+    public Object getValueFromIssue(CustomField customField, Issue issue) {
+        if (issue != null && issue.getKey() != null) {
+            return issue.getKey() + " " + issue.getSummary();
+        } else {
             return "";
         }
     }
 
-    public Map<String, Object> getVelocityParameters(
-        Issue issue,
-        CustomField field,
-        FieldLayoutItem fieldLayoutItem)
-    {
+    public Map<String, Object> getVelocityParameters(Issue issue, CustomField field, FieldLayoutItem fieldLayoutItem) {
+        @SuppressWarnings("unchecked")
         Map<String, Object> params = super.getVelocityParameters(issue, field, fieldLayoutItem);
-        if (issue != null && issue.getId() != null)
-        {
+        if (issue != null && issue.getId() != null) {
             params.put("issue_id", issue.getId());
         }
         return params;
